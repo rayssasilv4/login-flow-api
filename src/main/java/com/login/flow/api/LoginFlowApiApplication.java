c5e081f05ebf1f66364297fce11902ff5db6a568
alexandresjunior <souzajr.alexandre@gmail.com>
2023-04-16 10:34:51 -0300

    Criação de projeto spring boot

---
+++ src/main/java/com/login/flow/api/LoginFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.login.flow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginFlowApiApplication.class, args);
+	}
+
+}
